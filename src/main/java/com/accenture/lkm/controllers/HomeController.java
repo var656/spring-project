a88a327b6c47ed5ed5f8c08ed92606ac54155577
Varun Sharma <varun.vineet.sharma@accenture.com>
2019-01-08 10:22:15 +0530

    Run

--- src/main/java/com/accenture/lkm/controllers/HomeController.java
+++ src/main/java/com/accenture/lkm/controllers/HomeController.java
@@ -12,7 +12,7 @@ public class HomeController {
 	public ModelAndView home() {
 		ModelAndView mv = new ModelAndView("index");
 		mv.addObject("title", "Accenture - High performance. Delivered");
-		mv.addObject("greeting", "Welcome in the world of Pipeline1!");
+		mv.addObject("greeting", "Welcome in the world of Pipeline");
 		return mv;
 	}
 	
